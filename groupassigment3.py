685f49775458dd530bc85dc092b8b92d1c58defc
Luke McConnell <56060247+lmcconnell1665@users.noreply.github.com>
2020-10-19 13:25:03 -0500

    Add files via upload

---
+++ groupassigment3.py
@@ -0,0 +1,7 @@
+#Group assignment 3
+
+#Use the data, inputs and outputs as specified in group assignment 2.
+
+#Part 1: Make the best possible model, as measured on a validation set. You can use anything we have covered up until and including session 17. Better validation performance translates to a higher grade on the assignment.
+
+#Part 2: Coming soon 
